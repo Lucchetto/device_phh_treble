4cc150fafd2e71488761656390074f260c6d9fe0
Lucchetto <32309609+Lucchetto@users.noreply.github.com>
2020-01-05 11:50:52 +0100

    phhgsi_arm64_ab: increase dynamic system reserved space

--- phhgsi_arm64_ab/BoardConfig.mk
+++ phhgsi_arm64_ab/BoardConfig.mk
@@ -1,6 +1,8 @@
 include build/make/target/board/generic_arm64_ab/BoardConfig.mk
 include device/phh/treble/board-base.mk
 
+BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 393216000
+
 ifeq ($(BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE),)
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2147483648
 endif
